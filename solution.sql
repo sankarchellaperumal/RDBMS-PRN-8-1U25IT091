eb06b4b96165ad074458c2e8c4b7e815c8ab3531
sankarchellaperumal <csankar90474318@gmail.com>
2026-08-27 09:19:17 +0530

    PROGRAM 8 RDBMS 1U25IT091

--- solution.sql
+++ solution.sql
@@ -1,1 +1,12 @@
 
+SELECT COUNT(Salary) AS TotalEmployees
+FROM Employee;
+
+SELECT MAX(Salary) AS HighestSalary
+FROM Employee;
+
+SELECT MIN(Salary) AS LowestSalary
+FROM Employee;
+
+SELECT AVG(Salary) AS AverageSalary
+FROM Employee;
